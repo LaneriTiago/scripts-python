ab14a53bd6dac77264a45d5e4cdea0d0e19002d5
Tiago Luis Laneri <tiago.laneri93@gmail.com>
2020-09-13 20:25:03 -0300

    aula 4 - mack

---
+++ mackenzie/aula04-exercicio.py
@@ -0,0 +1,22 @@
+#Um banco concederá um crédito especial a seus clientes de acordo com o saldo médio no último ano. Faça
+#um pseudocódigo que receba o saldo médio de um cliente e calcule o valor do crédito, de acordo com a tabela
+#a seguir. Mostre o saldo médio e o valor do crédito.
+
+saldo_medio = float (input('Digite o crédito: '))
+
+if saldo_medio > 400:
+    credito = saldo_medio * 0.3
+
+elif saldo_medio >= 300.01: 
+    credito = saldo_medio * 0.25
+
+elif saldo_medio >= 200.01:
+    credito = saldo_medio * 0.2
+    
+else: 
+    credito = saldo_medio * 0.1
+
+print ('Saldo medio:  R$ ', saldo_medio) 
+print ('Crédito:  R$ ', credito)
+
+    
